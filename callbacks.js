057dbabf18d5f55d982f17ded88f671da8aeb58a
Rafael de Sousa <sousarafael151@gmail.com>
2022-03-24 14:46:51 -0300

    ------

---
+++ callbacks.js
@@ -0,0 +1,13 @@
+function somar (valorA, valorB){
+    return valorA + valorB
+}
+somar(4,4)
+function subtrair (valor1, valor2){
+    return valor1 - valor2
+}
+subtrair(2, 4)
+//function calcular (valor, operador){
+//    return operador(valor)
+//}
+
+//calcular(2, somar(2, 4))
--- if-ternario.js
+++ if-ternario.js
@@ -1,2 +1,18 @@
-var nome = 'João'
-nome == 'joão' || 'Joao' || 'João' || 'joao'? console.log('Seja bem vindo senhor'): console.log(`Olá ${nome}`);
+var nome = 'Rafael'
+nome = 'joão' || 'Joao' || 'João' || 'joao'? console.log('Seja bem vindo senhor'): console.log(`Olá ${livro}`);
+
+/*var livros = [
+    {livro: 'A branca de neve'},
+    {livro: 'Hobbit'},
+    {livro: 'Harry potter'},
+    {livro: 'Sherlock Holmes'},
+    {livro: 'Com amor, simon'},
+    {livro: 'Mo dao zu shi'},
+    {livro: 'Senhor dos Anéis'}
+]*/
+
+//livros == 'Hobbit' && 'Senhor dos Anéis'? console.log('Parabéns, estes combo está em promoção!') : console.log('Deseja algo mais?');
+
+var idioma = 'Español'
+
+//idioma == 'English'? console.log('Hello, nice to meet you!') : console.log('Olá, tudo bem?')
